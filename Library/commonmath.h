670e8f602ffa7e5e558a686d9fc4958d9ac10270
DESKTOP-REBS1R9\John <JohnDTill@gmail.com>
2019-02-22 12:38:45 -0500

    Fixed comments which referenced se3 where so3 was intended

--- Library/commonmath.h
+++ Library/commonmath.h
@@ -7,7 +7,7 @@ namespace ContinuumRobotLibrary{
 
 const double pi = 3.1415926535897932384626433832795028841971L;
 
-/*! Maps a vector in R3 to a 3x3 skew-symettric matrix in se3. */
+/*! Maps a vector in R3 to a 3x3 skew-symettric matrix in so3. */
 inline Matrix3d hat(Vector3d y){
     Matrix3d y_hat;
     y_hat <<    0, -y(2),  y(1),
@@ -17,7 +17,7 @@ inline Matrix3d hat(Vector3d y){
     return y_hat;
 }
 
-/*! Maps a matrix in se3 to a vector in R3. The input should be skew-symmetric but is not validated. */
+/*! Maps a matrix in so3 to a vector in R3. The input should be skew-symmetric but is not validated. */
 inline Vector3d inv_hat(Matrix3d y_hat){
     Vector3d y;
     y << y_hat(2,1), y_hat(0,2), y_hat(1,0);
